d313d010cf12332b42b30a80538bd6207a0e6aa6
moviesview <anyaclaire0@gmail.com>
2026-09-05 17:59:42 +0530

    Add files via upload

---
+++ assets/thumbnail-map.js
@@ -0,0 +1,79 @@
+window.THIRAI_X_THUMBNAILS = {
+  "anbe diana": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/anbediana-20260309173441-24240.jpg",
+  "pyaar prema kalyanam": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/pyaarpremakalyanam-20260813115615-24720.jpg",
+  "jana nayagan": "https://images.filmibeat.com/ph-big/2026/07/jana-nayagan1784272316_0.jpg",
+  "nooru sami": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/noorusaami-20260316181746-24266.jpg",
+  "love oh love": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/loveohlove-20260326135213-24294.jpg",
+  "naaigal jaakirathai": "https://image.tmdb.org/t/p/original/8xrDcNfzIAD1NqqYuSnMg6qdyA7.jpg",
+  "salim": "https://commons.wikimedia.org/wiki/Special:FilePath/Salim_film_poster.jpg?width=500",
+  "dc": "https://poster.gsc.com.my/2026/260805_DC_big.jpg",
+  "g d n edison of india": "https://pbs.twimg.com/media/GkC_z1ZWwAA2zIh?format=jpg&name=large",
+  "arulvaan": "https://pbs.twimg.com/media/HLwo3t8bkAAOXnF.jpg",
+  "x men": "https://image.tmdb.org/t/p/w342/bRDAc4GogyS9ci3ow7UnInOcriN.jpg",
+  "x2 x men united": "https://klev.club/uploads/posts/2023-10/1697482622_klev-club-p-kartinki-lyudi-x-30.jpg",
+  "captain america the first avenger": "https://image.tmdb.org/t/p/w342/vSNxAJTlD0r02V9sPYpOjqDZXUK.jpg",
+  "the avengers": "https://image.tmdb.org/t/p/w342/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+
+  "spider man no way home": "https://image.tmdb.org/t/p/w342/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+  "doctor strange in the multiverse of madness": "https://image.tmdb.org/t/p/w342/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
+  "deadpool and wolverine": "https://image.tmdb.org/t/p/w342/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+  "black panther wakanda forever": "https://image.tmdb.org/t/p/w342/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+  "captain america brave new world": "https://image.tmdb.org/t/p/w342/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
+  "thunderbolts": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg",
+  "x men the last stand": "https://image.tmdb.org/t/p/w342/a2xicU8DpKtRizOHjQLC1JyCSRS.jpg",
+  "x men origins wolverine": "https://image.tmdb.org/t/p/w342/ucJroLJ6RCqXY6hjYeLCW6jbWwE.jpg",
+  "x men first class": "https://image.tmdb.org/t/p/w342/6vZHDdyKXhd2CezhWLmxLT9EP4s.jpg",
+  "x men days of future past": "https://image.tmdb.org/t/p/w342/tYfijzolzgoMOtegh1Y7j2Enorg.jpg",
+  "x men dark phoenix": "https://image.tmdb.org/t/p/w342/iIsMHA3gX9E7ceNgvjfu99vTF7v.jpg",
+  "x men apocalypse": "https://image.tmdb.org/t/p/w342/vsv91Fj4pxOEm2QJ96jq2EXS7QT.jpg",
+  "deadpool": "https://www.themoviedb.org/t/p/w780/fSRb7vyIP8rQpL0I47P3qUsEKX3.jpg",
+
+  "deadpool movie": "https://www.themoviedb.org/t/p/w780/fSRb7vyIP8rQpL0I47P3qUsEKX3.jpg",
+  "logan movie": "https://image.tmdb.org/t/p/w500/fnbjcRDYn6YviCcePDnGdyAkYsB.jpg",
+  "anbe diana movie": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/anbediana-20260309173441-24240.jpg",
+  "jana nayagan movie": "https://images.filmibeat.com/ph-big/2026/07/jana-nayagan1784272316_0.jpg",
+  "nooru sami movie": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/noorusaami-20260316181746-24266.jpg",
+  "love oh love movie": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/loveohlove-20260326135213-24294.jpg",
+  "naaigal jaakirathai movie": "https://image.tmdb.org/t/p/original/8xrDcNfzIAD1NqqYuSnMg6qdyA7.jpg",
+  "salim movie": "https://commons.wikimedia.org/wiki/Special:FilePath/Salim_film_poster.jpg?width=500",
+  "pyaar prema kalyanam movie": "https://images.filmibeat.com/img/280x383/popcorn/movie_posters/pyaarpremakalyanam-20260813115615-24720.jpg",
+  "deadpool 2016": "https://www.themoviedb.org/t/p/w780/fSRb7vyIP8rQpL0I47P3qUsEKX3.jpg",
+  "deadpool 2": "https://image.tmdb.org/t/p/w342/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg",
+  "logan": "https://image.tmdb.org/t/p/w500/fnbjcRDYn6YviCcePDnGdyAkYsB.jpg",
+  "logan 2017": "https://image.tmdb.org/t/p/w500/fnbjcRDYn6YviCcePDnGdyAkYsB.jpg",
+  "the wolverine": "https://image.tmdb.org/t/p/w342/xNi8daRmN4XY8rXHd4rwLbJf1cU.jpg",
+  "the amazing spider man": "https://image.tmdb.org/t/p/w342/jexoNYnPd6vVrmygwF6QZmWPFdu.jpg",
+  "the amazing spider man 2": "https://image.tmdb.org/t/p/w342/bU7nTmvmy0h3VUP01v1T2imgH6N.jpg",
+  "spider man": "https://image.tmdb.org/t/p/w342/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
+  "spider man 2": "https://image.tmdb.org/t/p/w342/olxpyq9kJAZ2NU1siLshhhXEPR7.jpg",
+  "spider man 3": "https://image.tmdb.org/t/p/w342/qFmwhVUoUSXjkKRmca5yGDEXBIj.jpg",
+  "thor": "https://image.tmdb.org/t/p/w342/q8pF6s9b9veTQvxTqMDIQf9nJKi.jpg",
+  "thor 2": "https://moviepostermexico.com/cdn/shop/products/thor_the_dark_world_xxlg_1024x1024%402x.jpg?v=1576002526",
+  "avengers infinity war": "https://image.tmdb.org/t/p/w342/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+  "avengers endgame": "https://image.tmdb.org/t/p/w342/ulzhLuWrPK07P1YkdWQLZnQh1JL.jpg"
+  ,"thunderbolts star": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg"
+  ,"thunderbolts star 2025": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg"
+  ,"captain america brave new world 2025": "https://image.tmdb.org/t/p/w342/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg"
+
+};
+
+
+// Online thumbnail fallbacks for hosts that may occasionally fail/hotlink-block.
+window.THIRAI_X_THUMBNAIL_FALLBACKS = {
+  "deadpool": [
+    "https://www.themoviedb.org/t/p/w780/fSRb7vyIP8rQpL0I47P3qUsEKX3.jpg",
+    "https://1.bp.blogspot.com/-oc69j5gbtKU/VRA4b1fB7SI/AAAAAAAAC_4/uYDFWtxCA18/s1600/Deadpool%2B2016.jpg"
+  ],
+  "deadpool 2016": [
+    "https://www.themoviedb.org/t/p/w780/fSRb7vyIP8rQpL0I47P3qUsEKX3.jpg",
+    "https://1.bp.blogspot.com/-oc69j5gbtKU/VRA4b1fB7SI/AAAAAAAAC_4/uYDFWtxCA18/s1600/Deadpool%2B2016.jpg"
+  ],
+  "salim": [
+    "https://commons.wikimedia.org/wiki/Special:FilePath/Salim_film_poster.jpg?width=500",
+    "https://en.wikipedia.org/wiki/Special:Redirect/file/Salim_film_poster.jpg?width=500"
+  ],
+  "salim movie": [
+    "https://commons.wikimedia.org/wiki/Special:FilePath/Salim_film_poster.jpg?width=500",
+    "https://en.wikipedia.org/wiki/Special:Redirect/file/Salim_film_poster.jpg?width=500"
+  ]
+};
