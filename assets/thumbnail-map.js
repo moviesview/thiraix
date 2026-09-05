e59dc4a616d78094ebd01a18f8a44a2167c5f207
moviesview <anyaclaire0@gmail.com>
2026-09-05 16:37:37 +0530

    Add files via upload

---
+++ assets/thumbnail-map.js
@@ -0,0 +1,36 @@
+window.THIRAI_X_THUMBNAILS = {
+  "x men": "https://image.tmdb.org/t/p/w342/bRDAc4GogyS9ci3ow7UnInOcriN.jpg",
+  "x2 x men united": "https://klev.club/uploads/posts/2023-10/1697482622_klev-club-p-kartinki-lyudi-x-30.jpg",
+  "captain america the first avenger": "https://image.tmdb.org/t/p/w342/vSNxAJTlD0r02V9sPYpOjqDZXUK.jpg",
+  "the avengers": "https://image.tmdb.org/t/p/w342/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+
+  "spider man no way home": "https://image.tmdb.org/t/p/w342/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+  "doctor strange in the multiverse of madness": "https://image.tmdb.org/t/p/w342/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
+  "deadpool and wolverine": "https://image.tmdb.org/t/p/w342/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+  "black panther wakanda forever": "https://image.tmdb.org/t/p/w342/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+  "captain america brave new world": "https://image.tmdb.org/t/p/w342/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
+  "thunderbolts": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg",
+  "x men the last stand": "https://image.tmdb.org/t/p/w342/a2xicU8DpKtRizOHjQLC1JyCSRS.jpg",
+  "x men origins wolverine": "https://image.tmdb.org/t/p/w342/ucJroLJ6RCqXY6hjYeLCW6jbWwE.jpg",
+  "x men first class": "https://image.tmdb.org/t/p/w342/6vZHDdyKXhd2CezhWLmxLT9EP4s.jpg",
+  "x men days of future past": "https://image.tmdb.org/t/p/w342/tYfijzolzgoMOtegh1Y7j2Enorg.jpg",
+  "x men dark phoenix": "https://image.tmdb.org/t/p/w342/iIsMHA3gX9E7ceNgvjfu99vTF7v.jpg",
+  "x men apocalypse": "https://image.tmdb.org/t/p/w342/vsv91Fj4pxOEm2QJ96jq2EXS7QT.jpg",
+  "deadpool": "https://image.tmdb.org/t/p/w342/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
+  "deadpool 2": "https://image.tmdb.org/t/p/w342/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg",
+  "logan": "https://image.tmdb.org/t/p/w342/fnbjcRDYn6YviCcePDnGdyAkYsB.jpg",
+  "the wolverine": "https://image.tmdb.org/t/p/w342/xNi8daRmN4XY8rXHd4rwLbJf1cU.jpg",
+  "the amazing spider man": "https://image.tmdb.org/t/p/w342/jexoNYnPd6vVrmygwF6QZmWPFdu.jpg",
+  "the amazing spider man 2": "https://image.tmdb.org/t/p/w342/bU7nTmvmy0h3VUP01v1T2imgH6N.jpg",
+  "spider man": "https://image.tmdb.org/t/p/w342/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
+  "spider man 2": "https://image.tmdb.org/t/p/w342/olxpyq9kJAZ2NU1siLshhhXEPR7.jpg",
+  "spider man 3": "https://image.tmdb.org/t/p/w342/qFmwhVUoUSXjkKRmca5yGDEXBIj.jpg",
+  "thor": "https://image.tmdb.org/t/p/w342/q8pF6s9b9veTQvxTqMDIQf9nJKi.jpg",
+  "thor 2": "https://moviepostermexico.com/cdn/shop/products/thor_the_dark_world_xxlg_1024x1024%402x.jpg?v=1576002526",
+  "avengers infinity war": "https://image.tmdb.org/t/p/w342/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+  "avengers endgame": "https://image.tmdb.org/t/p/w342/ulzhLuWrPK07P1YkdWQLZnQh1JL.jpg"
+  ,"thunderbolts star": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg"
+  ,"thunderbolts star 2025": "https://image.tmdb.org/t/p/w342/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg"
+  ,"captain america brave new world 2025": "https://image.tmdb.org/t/p/w342/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg"
+
+};
---
+++ assets/watch-order.js
@@ -0,0 +1,108 @@
+window.THIRAI_X_WATCH_ORDERS = [
+  {
+    id: "doomsday",
+    eyebrow: "AVENGERS: DOOMSDAY",
+    title: "Essential Watch List to See Avengers: Doomsday",
+    description: "The essential films and series to watch before Avengers: Doomsday.",
+    presentation: "Essential viewing order before Avengers: Doomsday",
+    banner: "https://lumiere-a.akamaihd.net/v1/images/image_433c0898.jpeg?region=0%2C0%2C2048%2C878",
+    items: [
+      "X-Men", "X2: X-Men United", "Captain America: The First Avenger", "The Avengers",
+      "Avengers: Infinity War", "Avengers: Endgame", "Loki S1", "Loki S2",
+      "Shang-Chi and the Legend of the Ten Rings", "Spider-Man: No Way Home",
+      "Doctor Strange in the Multiverse of Madness", "Black Panther: Wakanda Forever",
+      "Deadpool & Wolverine", "Captain America: Brave New World", "Thunderbolts",
+      "The Fantastic Four: First Steps"
+    ]
+  },
+  {
+    id: "mcu",
+    eyebrow: "MCU MOVIES - RELEASE ORDER",
+    title: "Marvel Cinematic Universe (MCU) Movies - Release Order",
+    description: "Marvel Cinematic Universe movies arranged by their theatrical release order.",
+    presentation: "Presented in MCU movie release order",
+    banner: "https://images.hdqwalls.com/download/avengers-endgame-banner-uw-1920x1080.jpg?dl=1",
+    items: [
+      "Iron Man",
+      "The Incredible Hulk",
+      "Iron Man 2",
+      "Thor",
+      "Captain America: The First Avenger",
+      "The Avengers",
+      "Iron Man 3",
+      "Thor: The Dark World",
+      "Captain America: The Winter Soldier",
+      "Guardians of the Galaxy",
+      "Avengers: Age of Ultron",
+      "Ant-Man",
+      "Captain America: Civil War",
+      "Doctor Strange",
+      "Guardians of the Galaxy Vol. 2",
+      "Spider-Man: Homecoming",
+      "Thor: Ragnarok",
+      "Black Panther",
+      "Avengers: Infinity War",
+      "Ant-Man and the Wasp",
+      "Captain Marvel",
+      "Avengers: Endgame",
+      "Spider-Man: Far From Home",
+      "Black Widow",
+      "Shang-Chi and the Legend of the Ten Rings",
+      "Eternals",
+      "Spider-Man: No Way Home",
+      "Doctor Strange in the Multiverse of Madness",
+      "Thor: Love and Thunder",
+      "Black Panther: Wakanda Forever",
+      "Ant-Man and the Wasp: Quantumania",
+      "Guardians of the Galaxy Vol. 3",
+      "The Marvels",
+      "Deadpool & Wolverine",
+      "Captain America: Brave New World",
+      "Thunderbolts",
+      "The Fantastic Four: First Steps"
+    ]
+  },
+  {
+    id: "xmen",
+    eyebrow: "X-MEN FILM SERIES",
+    title: "X-Men Film Series - In Order",
+    description: "All X-Men, Wolverine and Deadpool films arranged in the same release order as the reference list.",
+    presentation: "Presented in release order",
+    banner: "https://images5.alphacoders.com/525/thumb-1920-525731.jpg",
+    items: [
+      "X-Men",
+      "X2: X-Men United",
+      "X-Men: The Last Stand",
+      "X-Men Origins: Wolverine",
+      "X-Men: First Class",
+      "The Wolverine",
+      "X-Men: Days of Future Past",
+      "Deadpool",
+      "X-Men: Apocalypse",
+      "Logan",
+      "Deadpool 2",
+      "X-Men: Dark Phoenix",
+      "The New Mutants",
+      "Deadpool & Wolverine"
+    ]
+  }
+  ,{
+    id: "spiderman",
+    eyebrow: "SPIDER-MAN",
+    title: "Spider-Man Trilogy",
+    description: "Sam Raimi's Spider-Man films with Tobey Maguire.",
+    presentation: "Presented in release order: Spider-Man 1 → 2 → 3",
+    banner: "https://images.squarespace-cdn.com/content/574bec3527d4bdfe229ea099/1536286716371-DOBK663HIDUISILTLPHG/spider-man-2002-1108x0-c-default.jpg?content-type=image%2Fjpeg",
+    items: ["Spider-Man", "Spider-Man 2", "Spider-Man 3"]
+  },
+  {
+    id: "amazing-spiderman",
+    eyebrow: "THE AMAZING SPIDER-MAN",
+    title: "The Amazing Spider-Man Collection",
+    description: "Andrew Garfield's Amazing Spider-Man films.",
+    presentation: "Presented in release order: The Amazing Spider-Man 1 → 2",
+    banner: "https://image.tmdb.org/t/p/original/sLWUtbrpiLp23a0XDSiUiltdFPJ.jpg",
+    items: ["The Amazing Spider-Man", "The Amazing Spider-Man 2"]
+  }
+
+];
